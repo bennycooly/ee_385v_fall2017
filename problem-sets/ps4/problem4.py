b0b1d0f8e3fed4d5ab1f24200602aa5d49ef7508
Ben Fu <ben_fu1@yahoo.com>
2017-11-05 00:41:31 -0500

    Add plots for problem4

--- problem-sets/ps4/problem4.py
+++ problem-sets/ps4/problem4.py
@@ -4,49 +4,59 @@ from scipy.integrate import quad
 from scipy.signal import argrelextrema
 import numpy as np
 import matplotlib.pyplot as plt
+from numpy import linalg as la
 import math
 import random
+from scipy.linalg import circulant
 
 # returns the roots of unity
-def roots(j_range):
-    N = j_range.size
-    res = np.copy(j_range)
-    for j in np.nditer(res, op_flags=["readwrite"]):
-        # print(j)
-        j[...] = pow(math.exp((2 * np.pi * np.sqrt(-1) * j) / N), j)
+def roots(j, N):
+    res = np.arange(0, N)
+    for i in np.nditer(res, op_flags=["readwrite"]):
+        # print(i)
+        i[...] = pow(math.exp((2 * np.pi * j) / N), i)
 
     return res
 
 def eigenvalues(j_range, c):
-    print(j_range.size)
     res = np.copy(j_range)
     for j in np.nditer(res, op_flags=["readwrite"]):
-        j[...] = np.dot(roots(j_range), c)
+        j[...] = np.inner(roots(j, j_range.size), c)
     print(res)
     return res
 
 
 
 def main():
-    N = 5
+    N = 100
     c = np.zeros(N)
-    c_val = 2
+    c_val = 1
     c[1] = c_val
     c[N-1] = c_val
     j = np.arange(0, N)
-    # j = np.linspace(0, N, N)
-    print(c)
-    print(j)
-    print(roots(j))
-    print(eigenvalues(j, c))
+
+    print(circulant(c))
+    print(la.eigvals(circulant(c)).argsort()[::-1])
 
     x = np.linspace(0, 1, 1000)
     plt.figure(1, figsize=(10, 8))
     plt.subplot(421)
-    plt.title("Problem 4a: Eigenvalue Spectrum")
+    plt.title("Problem 4a: Eigenvalue Spectrum, N=" + str(N))
     plt.ylabel("Eigenvalue")
-    plt.xlabel("j")
-    plt.plot(j, eigenvalues(j, c))
+    plt.xlabel("Index (j)")
+    plt.plot(j, la.eigvals(circulant(c)).argsort()[::-1], label="c=" + str(c_val))
+    c_val = 2
+    c[1] = c_val
+    c[N-1] = c_val
+    plt.plot(j, la.eigvals(circulant(c)).argsort()[::-1], label="c=" + str(c_val))
+    c_val = -2
+    c[1] = c_val
+    c[N-1] = c_val
+    plt.plot(j, la.eigvals(circulant(c)).argsort()[::-1], label="c=" + str(c_val))
+    c_val = -200
+    c[1] = c_val
+    c[N-1] = c_val
+    plt.plot(j, la.eigvals(circulant(c)).argsort()[::-1], label="c=" + str(c_val))
     # plt.plot(x, L(x, beta, gamma), label="β = 4 (infinite local minima)")
     # beta = 3.9
     # plt.plot(x, L(x, beta, gamma), label="β = 3.9 (1 local minima)")
